850fb7464a7e14de06ed6acac562d4d7272e796b
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 22:20:00 +0000

    [task 15] documentation

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-
+    Module to slice a matrix
 """
 
 
 def np_slice(matrix, axes={}):
-    """"""
+    """Function that will slice a matrix"""
     sliced = []
     max_key = max(axes)
     for i in range(max_key + 1):
