b3f576335389c17c43f0b0cc2c571437081e9f42
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 19:15:08 +0000

    [task 14]

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Module to perform the multiplication of 2 matrices
+    using numpy
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Function that perform the multiplication of 2 matrices
+    using numpy"""
+    return np.matmul(mat1, mat2)
