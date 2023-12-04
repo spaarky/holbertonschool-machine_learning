95f4dd8f09aebb4fd55300fc25bb658c00cdc2be
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 14:23:46 +0000

    [task 5] init without permissions

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Module to add 2D matrices
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """Function that will add 2 matrices"""
+    if len(mat1) != len(mat2):
+        return None
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    else:
+        return [[mat1[i][j] + mat2[i][j] for i in range(len(mat1[0]))] for j in range(len(mat2))]
