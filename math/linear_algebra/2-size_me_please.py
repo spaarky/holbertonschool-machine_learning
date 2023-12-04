654b680b86a41cd0fe11cab0bd1f604e5a6e2c2d
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 13:18:14 +0000

    [task 2] more documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+    Module to calculate the dimensions of a matrix
+"""
 def matrix_shape(matrix):
     """Function that will calculate the dimensions of a matrix"""
     if type(matrix[0]) != list:
