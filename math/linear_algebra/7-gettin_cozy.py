869625cdca4acbde7506e4ffd865f47a74cef6a6
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 17:51:59 +0000

    [task 7] documentation

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-
+    Module to concatenate 2 matrices
 """
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    """"""
+    """Function to concat 2 matrices"""
     if (len(mat1[0]) == len(mat2[0])) and axis == 0:
         concat = [element.copy() for element in mat1]
         concat += [element.copy() for element in mat2]
