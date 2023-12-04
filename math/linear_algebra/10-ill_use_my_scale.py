3f93763f26643cbc88056f25d481cd237716b870
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 18:47:38 +0000

    [task 10] no import and documentation

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-
+    Module to get the shape of a matrix
 """
-import numpy as np
+
 
 def np_shape(matrix):
-    """"""
+    """Function that will return the shape of a matrix"""
     return matrix.shape
