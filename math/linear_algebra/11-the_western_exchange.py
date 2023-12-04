d87d01cc3ca3909daa13b02faa63eb6891c29a3b
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 18:54:07 +0000

    [task 11]

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+    Module to get the transpose of a matrix using numpy
+"""
+
+def np_transpose(matrix):
+    """Function that will return the transpose of a matrix using numpy"""
+    return matrix.transpose()
