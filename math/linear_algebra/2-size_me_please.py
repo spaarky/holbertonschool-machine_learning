2cc20e94945be366db7ec58a3ddc427cb4e67f30
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 13:16:19 +0000

    [task 2] update permissions and recursion

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
     """Function that will calculate the dimensions of a matrix"""
-    shape = []
-    shape.append(int(len(matrix)))
-    if isinstance(matrix[0], list):
-        shape.append(int(len(matrix[0])))
-    if isinstance(matrix[0][0], list):
-        shape.append(int(len(matrix[0][0])))
-    return shape
+    if type(matrix[0]) != list:
+        return [len(matrix)]
+    else:
+        return [len(matrix)] + matrix_shape(matrix[0])
