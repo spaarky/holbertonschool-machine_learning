10d1130ae36e4290640183f408e4172b7d73f42a
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 22:32:46 +0000

    [task 16] documentation

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-
+    Module to calculate the sum of 2 matrices even 3D
 """
 
 
 def add_matrices(mat1, mat2):
-    """"""
+    """Main function to calculate the addition
+    of 2 matrices"""
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
     else:
@@ -13,7 +14,8 @@ def add_matrices(mat1, mat2):
 
 
 def add_recursive(mat1, mat2):
-    """"""
+    """Calculate the addition of 2 matrices
+    using recursion"""
     if type(mat1[0]) != list:
         return [mat1[index] + mat2[index] for index in range(len(mat1))]
     else:
@@ -25,10 +27,7 @@ def add_recursive(mat1, mat2):
 
 
 def matrix_shape(matrix):
-    """
-    Needs a matrix as input
-    Returns the shape as a list of integers
-    """
+    """Return the shape of a matrix"""
     if type(matrix[0]) is not list:
         return [len(matrix)]
     else:
