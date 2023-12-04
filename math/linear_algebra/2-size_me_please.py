d94015331b730f65334338390ecd3060f4268f7c
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 13:04:36 +0000

    task 2 documentation and type of the item from string to int

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """Function that will calculate the dimensions of a matrix"""
     shape = []
-    shape.append(len(matrix))
-    shape.append(len(matrix[0]))
+    shape.append(int(len(matrix)))
+    shape.append(int(len(matrix[0])))
     if isinstance(matrix[0][0], list):
-        shape.append(len(matrix[0][0]))
+        shape.append(int(len(matrix[0][0])))
     return shape
