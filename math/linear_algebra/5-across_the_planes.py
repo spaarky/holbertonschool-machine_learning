a55c7186b0827b97847560c6e8a321a20809b4ee
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 14:33:03 +0000

    [task 5] remove else statement

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -10,6 +10,5 @@ def add_matrices2D(mat1, mat2):
         return None
     if len(mat1[0]) != len(mat2[0]):
         return None
-    else:
-        return [[(mat1[i][j] + mat2[i][j]) for i in range(len(mat1[0]))]
-                for j in range(len(mat1))]
+    return [[(mat1[i][j] + mat2[i][j]) for j in range(len(mat1[0]))]
+            for i in range(len(mat1))]
