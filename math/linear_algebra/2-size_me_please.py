4ccad6b1a7aa6b18d7e6f82818d705b63066e93b
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-04 13:09:12 +0000

    condition for 2D matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,7 +3,8 @@ def matrix_shape(matrix):
     """Function that will calculate the dimensions of a matrix"""
     shape = []
     shape.append(int(len(matrix)))
-    shape.append(int(len(matrix[0])))
+    if isinstance(matrix[0], list):
+        shape.append(int(len(matrix[0])))
     if isinstance(matrix[0][0], list):
         shape.append(int(len(matrix[0][0])))
     return shape
