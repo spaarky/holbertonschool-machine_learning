bbd600112b06fcc3c6148d69cad73607b4452f64
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-11 12:04:34 +0000

    [task 17] make float int when needed + doc

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-
+    Module to calculate the integral of a polynomial
 """
 
 
 def poly_integral(poly, C=0):
-    """"""
+    """Function to calculate the integral of a polynomial"""
     if type(poly) is not list or poly == []:
         return None
     if type(C) is not int:
@@ -15,4 +15,5 @@ def poly_integral(poly, C=0):
     for i in range(len(poly)):
         poly[i] = poly[i] / (i + 1)
     poly.insert(0, C)
-    return poly
+    res = [int(x) if isinstance(x, float) and x.is_integer() else x for x in poly]
+    return res
