10fcb2592ba0d53dbdaee882bc4534e6466faf7a
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-11 12:06:40 +0000

    [task 17] error when poly is [0]

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -14,6 +14,8 @@ def poly_integral(poly, C=0):
     # Find primitive
     for i in range(len(poly)):
         poly[i] = poly[i] / (i + 1)
-    poly.insert(0, C)
-    res = [int(x) if isinstance(x, float) and x.is_integer() else x for x in poly]
+    if poly is not [0]:
+        poly.insert(0, C)
+    res = [int(x) if isinstance(x, float) and x.is_integer()
+           else x for x in poly]
     return res
