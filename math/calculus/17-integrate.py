3dae70d7e174081aea988189fc1c725311a52a81
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-11 12:32:19 +0000

    [task 17] fix for poly is [0]

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -10,12 +10,16 @@ def poly_integral(poly, C=0):
         return None
     if type(C) is not int:
         return None
+    if poly == [0]:
+        if C is not 0:
+            return [C]
+        else:
+            return [0]
 
     # Find primitive
     for i in range(len(poly)):
         poly[i] = poly[i] / (i + 1)
-    if poly is not [0]:
-        poly.insert(0, C)
+    poly.insert(0, C)
     res = [int(x) if isinstance(x, float) and x.is_integer()
            else x for x in poly]
     return res
