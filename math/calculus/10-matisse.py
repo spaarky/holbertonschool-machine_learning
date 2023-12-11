ac33930d5d22c7d1294835407d95b13cf39dcf71
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-11 09:20:12 +0000

    [task 10] documentation and error handling

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 """
-
+    Module to derivate polynomials
 """
 
 
 def poly_derivative(poly):
-    """"""
+    """Function to derivate polynomials"""
+    if type(poly) is not list or poly == []:
+        return None
+    if len(poly) < 1:
+        return [0]
+
     for i in range(len(poly)):
         poly[i] = poly[i] * i
     poly.pop(0)
