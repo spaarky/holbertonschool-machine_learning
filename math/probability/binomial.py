8b69a81a519b396a0ca66577d5fb686ee8dfde6f
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-18 12:13:43 +0000

    [binomial] p out of range correction

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -11,7 +11,7 @@ class Binomial:
         if data is None:
             if n < 1:
                 raise ValueError('n must be a positive value')
-            if p >= 0 or p <= 1:
+            if p >= 1 or p <= 0:
                 raise ValueError('p must be greater that 0 and less than 1')
             self.p = float(p)
             self.n = int(n)
