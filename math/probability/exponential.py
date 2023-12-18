4fd58708c8932e0db9e1d81f2506f81aead58c00
Spark4545 <lerouxpaulhenri@gmail.com>
2023-12-18 09:07:14 +0000

    [Exponential]

---
+++ math/probability/exponential.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Representing exponential distribution
+"""
+
+
+class Exponential:
+    """Class exponential Distribution"""
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """Class contructor"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            elif len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = 1 / (sum(data) / len(data))
+
+    def pdf(self, x):
+        """Probability Mass Function for Exponential"""
+        if x < 0:
+            return 0
+        pdf = self.lambtha * Exponential.e ** (- self.lambtha * x)
+        return pdf
+
+    def cdf(self, x):
+        """Cummulative Distribution Function for Exponential"""
+        if x < 0:
+            return 0
+        cdf = 0
+        for i in range(x + 1):
+            cdf = 1 - Exponential.e ** (- self.lambtha * x)
+        return cdf
