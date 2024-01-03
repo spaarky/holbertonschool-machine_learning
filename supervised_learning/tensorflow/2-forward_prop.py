c118b90d5698207b888b8aea973aa86d6472c1dc
Spark4545 <lerouxpaulhenri@gmail.com>
2024-01-03 08:14:32 +0000

    [task 2] pycodestyle

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -10,12 +10,15 @@ def forward_prop(x, layer_sizes=[], activations=[]):
 
     Args:
         x (tf.placeholder): placeholder for the input data
-        layer_sizes (list, optional): contains the nulmber of nodes in each layer of the network. Defaults to [].
-        activations (list, optional): contains the activation functions for each layer of the network. Defaults to [].
+        layer_sizes (list, optional): contains the nulmber of nodes in
+            each layer of the network. Defaults to [].
+        activations (list, optional): contains the activation functions
+            for each layer of the network. Defaults to [].
     """
     for i in range(len(layer_sizes)):
         if i == 0:
             prediction = create_layer(x, layer_sizes[0], activations[0])
         else:
-            prediction = create_layer(prediction, layer_sizes[i], activations[i])
+            prediction = create_layer(prediction, layer_sizes[i],
+                                      activations[i])
     return prediction
