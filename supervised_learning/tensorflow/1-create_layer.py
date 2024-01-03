1506d5051392f689864c39745f916abb2887f647
Spark4545 <lerouxpaulhenri@gmail.com>
2024-01-03 07:44:23 +0000

    [task 1] trying to push without test because tensorflow is shit

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""summary
+"""
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """Write a function that return the output of the layer
+
+    Args:
+        prev (float): tensor output of the previous layer
+        n (integer): number of nodes in the layer to create
+        activation (function): activation function to use in the layer
+
+    Returns:
+        ?: tensor output of the layer
+    """
+
+    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.layers.Dense(n, activation=activation, kernel_initializer=init, name='layer')
+
+    return layer(prev)
