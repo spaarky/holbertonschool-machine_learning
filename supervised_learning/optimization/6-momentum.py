f0a7ca1e00bd849759b76ab18802cae988905b28
Spark4545 <lerouxpaulhenri@gmail.com>
2024-01-17 18:34:37 +0100

    [task 6] pycode

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -3,6 +3,7 @@
 """
 import tensorflow.compat.v1 as tf
 
+
 def create_momentum_op(loss, alpha, beta1):
     """Function creates the training operation for a neural network in
         tensorflow using the gradient descent with momentum
