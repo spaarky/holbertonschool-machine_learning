a8ca735a41a7b2b570a0e47720802bc9eb783ee0
Spark4545 <lerouxpaulhenri@gmail.com>
2024-01-22 17:02:10 +0000

    [task 1] doc

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -5,7 +5,14 @@ import numpy as np
 
 
 def sensitivity(confusion):
+    """Function to calculate the sensitivity of all classes
 
+    Args:
+        confusion (numpy.ndarray): confusion matrix
+
+    Returns:
+        (numpy.ndarray): sensitivity matrix
+    """
     # sensitivity formula: TP / P
     TP = np.diagonal(confusion)
     P = np.sum(confusion, axis=1)
