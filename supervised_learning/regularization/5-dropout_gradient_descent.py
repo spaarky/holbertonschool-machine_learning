f1611e059b905e898c545d7fafa8bcb85f263fb3
Spark4545 <lerouxpaulhenri@gmail.com>
2024-01-28 22:39:57 +0000

    [task 5] new way

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -20,23 +20,13 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
         L (integer): number of layers of the network
     """
     m = Y.shape[1]
-    for i in reversed(range(L)):
-        key_w = 'W' + str(i + 1)
-        key_b = 'b' + str(i + 1)
-        key_cache = 'A' + str(i + 1)
-        key_cache_dw = 'A' + str(i)
-        A = cache[key_cache]
-        A_dw = cache[key_cache_dw]
-        if i == L - 1:
-            dz = A - Y
-            W = weights[key_w]
-        else:
-            da = 1 - (A * A)
-            dz = np.matmul(W.T, dz)
-            dz = dz * da * cache["D{}".format(i + 1)]
-            dz = dz / keep_prob
-            W = weights[key_w]
-        dw = np.matmul(A_dw, dz.T) / m
+    dz = cache['A' + str(L)] - Y
+    for i in range(L, 0, -1):
+        dw = np.matmul(dz, cache['A' + str(i - 1)].T) / m
         db = np.sum(dz, axis=1, keepdims=True) / m
-        weights[key_w] = weights[key_w] - alpha * dw.T
-        weights[key_b] = weights[key_b] - alpha * db
+        dz = np.matmul(weights['W' + str(i)].T, dz) * (
+            1 - cache["A" + str(i - 1)] ** 2)
+        if i > 1:
+            dz *= cache['D' + str(i - 1)] / keep_prob
+        weights['W' + str(i)] -= alpha * dw
+        weights['b' + str(i)] -= alpha * db
