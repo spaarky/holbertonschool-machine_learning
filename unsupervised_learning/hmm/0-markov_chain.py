74e87fb60fad1ecc1921e5d2e74112e7856bb81d
Spark4545 <lerouxpaulhenri@gmail.com>
2024-04-07 22:20:26 +0000

    [task 0 & 1] init

---
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+import numpy as np
+
+
+def markov_chain(P, s, t=1):
+    """Determines the probability of a markov chain being in a particular state after a specified number of iterations
+
+    Args:
+        P (numpy.ndarray): shape (n, n) representing the transition matrix
+        s (numpy.ndarray): shape (1, n) representing the probability of starting in each state
+        t (integer, optional): number of iterations that the markov chain has been through. Defaults to 1.
+
+    Returns:
+        numpy.ndarray: shape (1, n) representing the probability of being in a specific state after t iterations, or None on failure
+    """
+    # Check errors
+    if type(P) is not np.ndarray:
+        return None
+    if len(P.shape) != 2:
+        return None
+    n, n_t = P.shape
+    if n != n_t:
+        return None
+    if type(s) is not np.ndarray:
+        return None
+    if len(s.shape) != 2 or s.shape[0] != 1 or s.shape[1] != n:
+        return None
+    if type(t) != int or t < 1:
+        return None
+    sum_test = np.sum(P, axis=1)
+    for elem in sum_test:
+        if not np.isclose(elem, 1):
+            return None
+
+    sn_t = s
+    sn = np.zeros((1, n))
+    for i in range(t):
+        sn = np.matmul(sn_t, P)
+        sn_t = sn
+
+    return sn
---
+++ unsupervised_learning/hmm/1-regular.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+import numpy as np
+
+
+def regular(P):
+    """Determines the steady state probabilities of a regular markov chain
+
+    Args:
+        P (numpy.ndarray): shape (n, n) representing the transition matrix
+
+    Returns:
+        numpy.ndarray: shape (1, n) containing the steady state probabilities, or None on failure
+    """
+
+    if type(P) is not np.ndarray:
+        return None
+    if len(P.shape) != 2:
+        return None
+    n, n_t = P.shape
+    if n != n_t:
+        return None
+    sum_test = np.sum(P, axis=1)
+    for elem in sum_test:
+        if not np.isclose(elem, 1):
+            return None
+
+    evals, evecs = np.linalg.eig(P.T)
+    stationary = evecs / evecs.sum()
+    stationary = stationary.real
+
+    for i in np.dot(stationary.T, P):
+        if (i >= 0).all() and np.isclose(i.sum(), 1):
+            return i.reshape(1, n)
+
+    return None
