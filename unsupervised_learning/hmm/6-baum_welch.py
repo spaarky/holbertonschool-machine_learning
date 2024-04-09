27f4a01a5ead4cf2aaa1664f12ac5b77f936045d
Spark4545 <lerouxpaulhenri@gmail.com>
2024-04-09 12:11:44 +0000

    [task 6] function fix

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -17,28 +17,8 @@ def backward(Observation, Emission, Transition, Initial):
         _type_: _description_
     """
 
-    if type(Observation) is not np.ndarray or len(Observation.shape) != 1:
-        return None, None
-    if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
-        return None, None
-    if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
-        return None, None
-    if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
-        return None, None
-    sum_test = np.sum(Emission, axis=1)
-    if not (sum_test == 1.0).all():
-        return None, None
-    sum_test = np.sum(Transition, axis=1)
-    if not (sum_test == 1.0).all():
-        return None, None
-    sum_test = np.sum(Initial, axis=0)
-    if not (sum_test == 1.0).all():
-        return None, None
-
     N, M = Emission.shape
     T = Observation.shape[0]
-    if N != Transition.shape[0] or N != Transition.shape[1]:
-        return None, None
 
     beta = np.zeros((N, T))
     beta[:, T - 1] = np.ones((N))
@@ -49,9 +29,7 @@ def backward(Observation, Emission, Transition, Initial):
                                     Transition[row, :] *
                                     Emission[:, Observation[col + 1]])
 
-    P = np.sum(Initial[:, 0] * Emission[:, Observation[0]] * beta[:, 0])
-
-    return P, beta
+    return beta
 
 
 def forward(Observation, Emission, Transition, Initial):
@@ -73,42 +51,18 @@ def forward(Observation, Emission, Transition, Initial):
             probabilities
     """
 
-    if type(Observation) is not np.ndarray or len(Observation.shape) != 1:
-        return None, None
-    if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
-        return None, None
-    if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
-        return None, None
-    if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
-        return None, None
-
-    sum_test = np.sum(Emission, axis=1)
-    if not (sum_test == 1).all():
-        return None, None
-    sum_test = np.sum(Transition, axis=1)
-    if not (sum_test == 1).all():
-        return None, None
-    sum_test = np.sum(Initial, axis=0)
-    if not (sum_test == 1).all():
-        return None, None
-
-    N, _ = Emission.shape
+    N, M = Emission.shape
     T = Observation.shape[0]
 
-    if N != Transition.shape[0] or N != Transition.shape[1]:
-        return None, None
-
-    F = np.zeros((N, T))
-    F[:, 0] = Initial.T * Emission[:, Observation[0]]
+    alpha = np.zeros((N, T))
+    alpha[:, 0] = Initial.T * Emission[:, Observation[0]]
 
     for col in range(1, T):
         for row in range(N):
-            aux = F[:, col - 1] * Transition[:, row]
-            F[row, col] = np.sum(aux * Emission[row, Observation[col]])
+            aux = alpha[:, col - 1] * Transition[:, row]
+            alpha[row, col] = np.sum(aux * Emission[row, Observation[col]])
 
-    P = np.sum(F[:, -1])
-
-    return P, F
+    return alpha
 
 
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
