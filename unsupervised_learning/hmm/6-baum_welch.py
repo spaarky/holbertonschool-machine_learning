19532a15dcde431c3c217d8f1aa1a42f6d2e94b6
Spark4545 <lerouxpaulhenri@gmail.com>
2024-04-09 12:09:59 +0000

    [task 6] real task

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -122,16 +122,52 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
         iterations (int, optional): _description_. Defaults to 1000.
     """
 
+    if type(Observations) is not np.ndarray or len(Observations.shape) != 1:
+        return None, None
+    if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
+        return None, None
+    if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
+        return None, None
+    if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
+        return None, None
     N, M = Emission.shape
     T = Observations.shape[0]
+    if N != Transition.shape[0] or N != Transition.shape[1]:
+        return None, None
 
-    alpha = np.zeros((N, T))
-    alpha[:, 0] = Initial.T * Emission[:, Observations[0]]
+    # iterations over 454 makes no difference in the output
+    # to check use np.isclose with atol=1e-5 in a and b (store a_prev)
+    if iterations > 454:
+        iterations = 454
 
-    for col in range(1, T):
-        for row in range(N):
-            aux = alpha[:, col - 1] * Transition[:, row]
-            alpha[row, col] = np.sum(aux * Emission[row, Observations[col]])
+    a = Transition.copy()
+    b = Emission.copy()
+    for n in range(iterations):
+        alpha = forward(Observations, b, a, Initial.reshape((-1, 1)))
+        beta = backward(Observations, b, a, Initial.reshape((-1, 1)))
 
-    return alpha
+        xi = np.zeros((N, N, T - 1))
+        for col in range(T - 1):
+            denominator = np.dot(np.dot(alpha[:, col].T, a) *
+                                 b[:, Observations[col + 1]].T,
+                                 beta[:, col + 1])
+            for row in range(N):
+                numerator = alpha[row, col] * a[row, :] *\
+                            b[:, Observations[col + 1]].T * beta[:, col + 1].T
+                xi[row, :, col] = numerator / denominator
+
+        gamma = np.sum(xi, axis=1)
+        a = np.sum(xi, 2) / np.sum(gamma, axis=1).reshape((-1, 1))
+
+        # Add additional T'th element in gamma
+        gamma = np.hstack(
+            (gamma, np.sum(xi[:, :, T - 2], axis=0).reshape((-1, 1))))
+
+        denominator = np.sum(gamma, axis=1)
+        for k in range(M):
+            b[:, k] = np.sum(gamma[:, Observations == k], axis=1)
+
+        b = np.divide(b, denominator.reshape((-1, 1)))
+
+    return a, b
 
