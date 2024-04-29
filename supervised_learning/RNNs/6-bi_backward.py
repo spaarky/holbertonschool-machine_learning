5ff37900efbd15837485613f466924f0c3ed1087
Spark4545 <lerouxpaulhenri@gmail.com>
2024-04-29 07:40:41 +0000

    [task 6] init

---
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""summary"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """summary"""
+    def __init__(self, i, h, o):
+        """Class constructor"""
+        self.Whf = np.random.normal(size=(h+i, h))
+        self.Whb = np.random.normal(size=(h+i, h))
+        self.Wy = np.random.normal(size=(2*h, o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """Performs forward propagation for one time step"""
+        # First, concat h_prev and x_t to match the shape of Wh
+        x_concat = np.concatenate((h_prev, x_t), axis=1)
+
+        h_next = np.tanh(np.matmul(x_concat, self.Whf) + self.bhf)
+
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """summary"""
+        # First, concat h_next and x_t to match the shape of Wh
+        x_concat = np.concatenate((h_next, x_t), axis=1)
+
+        h_prev = np.tanh(np.matmul(x_concat, self.Whb) + self.bhb)
+
+        return h_prev
