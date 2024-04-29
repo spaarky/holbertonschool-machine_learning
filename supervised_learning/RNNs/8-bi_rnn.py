59febea208169a8992f29f6e363476328b5023a4
Spark4545 <lerouxpaulhenri@gmail.com>
2024-04-29 07:52:58 +0000

    [task 8] pycode + opti

--- supervised_learning/RNNs/8-bi_rnn.py
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -5,24 +5,23 @@ import numpy as np
 
 def bi_rnn(bi_cell, X, h_0, h_t):
     """summary"""
-    t, m, i = X.shape
+    t, m, _ = X.shape
     h = h_0.shape[1]
-    H_forward = np.zeros((t, m, h))
-    H_backward = np.zeros((t, m, h))
+    H_for = np.zeros((t, m, h))
+    H_back = np.zeros((t, m, h))
     h_ft = h_0
     h_bt = h_t
-
     for step in range(t):
         x_ft = X[step]
-        x_bt = X[-(step - 1)]
+        x_bt = X[-(step + 1)]
 
         h_ft = bi_cell.forward(h_ft, x_ft)
         h_bt = bi_cell.backward(h_bt, x_bt)
 
-        H_forward[step] = h_ft
-        H_backward[-(step + 1)] = h_bt
+        H_for[step] = h_ft
+        H_back[-(step + 1)] = h_bt
 
-    H = np.concatenate((H_forward, H_backward), axis=-1)
+    H = np.concatenate((H_for, H_back), axis=-1)
     Y = bi_cell.output(H)
 
     return H, Y
