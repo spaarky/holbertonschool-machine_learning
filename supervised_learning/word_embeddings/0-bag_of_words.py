49832316822917e8dcccb505136622260cb73c50
Spark4545 <lerouxpaulhenri@gmail.com>
2024-05-15 12:08:42 +0000

    [task 0] no sklearn

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
-"""
-Creates a bag of words embedding matrix
-"""
-from sklearn.feature_extraction.text import CountVectorizer
+"""Summary"""
+import numpy as np
+import re
 
 
 def bag_of_words(sentences, vocab=None):
     """Summary"""
-    vector = CountVectorizer(vocabulary=vocab)
-    X = vector.fit_transform(sentences)
-    features = vector.get_feature_names()
-    embeddings = X.toarray()
+    Tokenised = [re.findall(r'\b\w+\b', sentences.lower()) for sentece in sentences]
 
-    return embeddings, features
+    if vocab is None:
+        vocab = sorted(set(word for sentence in Tokenised for word in sentence))
+
+    word_to_index = {word: i for i, word in enumerate(vocab)}
+
+    embeddings = np.zeros((len(Tokenised), len(vocab)), dtype=int)
+
+    for i, sentence in enumerate(Tokenised):
+        for word in sentence:
+            if word in word_to_index:
+                embeddings[i, word_to_index[word]] += 1
+
+    features = embeddings
+
+    return features, vocab
