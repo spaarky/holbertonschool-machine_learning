b4a8c4deb753644cfb29f9a1d0a3a755e523063b
Spark4545 <lerouxpaulhenri@gmail.com>
2024-05-15 12:10:06 +0000

    [task 0] update pycode

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
-"""Summary"""
+""" Creates a bag of words embedding matrix """
 import numpy as np
 import re
 
-
 def bag_of_words(sentences, vocab=None):
     """Summary"""
-    Tokenised = [re.findall(r'\b\w+\b', sentences.lower()) for sentece in sentences]
+    tokenized = [re.findall(r'\b\w+\b', sentence.lower()) for sentence in sentences]
 
     if vocab is None:
-        vocab = sorted(set(word for sentence in Tokenised for word in sentence))
+        vocab = sorted(set(word for sentence in tokenized for word in sentence))
 
-    word_to_index = {word: i for i, word in enumerate(vocab)}
+    word_to_index = {word: idx for idx, word in enumerate(vocab)}
 
-    embeddings = np.zeros((len(Tokenised), len(vocab)), dtype=int)
+    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
 
-    for i, sentence in enumerate(Tokenised):
+    for i, sentence in enumerate(tokenized):
         for word in sentence:
             if word in word_to_index:
                 embeddings[i, word_to_index[word]] += 1
 
-    features = embeddings
+    features = vocab
 
-    return features, vocab
+    return embeddings, features
