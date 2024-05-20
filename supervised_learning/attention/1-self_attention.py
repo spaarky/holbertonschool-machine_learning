a240ce846baa43a77e29a9d21007cdce065f36dc
Spark4545 <lerouxpaulhenri@gmail.com>
2024-05-20 21:08:05 +0000

    [task 1] real fix

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -15,7 +15,7 @@ class SelfAttention(tf.keras.layers.Layer):
     def call(self, s_prev, hidden_states):
         """Summary"""
         s_expanded = tf.expand_dims(s_prev, 1)
-        score = self.V(tf.nn.tanh(self.W(s_prev) + self.U(hidden_states)))
+        score = self.V(tf.nn.tanh(self.W(s_expanded) + self.U(hidden_states)))
 
         attention_weights = tf.nn.softmax(score, axis=1)
         context_vector = attention_weights * s_expanded
