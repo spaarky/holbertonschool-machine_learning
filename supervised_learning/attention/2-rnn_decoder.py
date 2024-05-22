ae6e6a67acd23aa34193b25e4a099e54e637497f
Spark4545 <lerouxpaulhenri@gmail.com>
2024-05-22 17:52:18 +0000

    [task 2] fix

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -5,28 +5,41 @@ SelfAttention = __import__('1-self_attention').SelfAttention
 
 
 class RNNDecoder(tf.keras.layers.Layer):
-    """Summary"""
+    """ Class RNNDecoder to decode for machine translation """
+
     def __init__(self, vocab, embedding, units, batch):
         """Summary"""
         super(RNNDecoder, self).__init__()
+
         self.embedding = tf.keras.layers.Embedding(
             input_dim=vocab,
             output_dim=embedding)
+
         self.gru = tf.keras.layers.GRU(
             units=units,
             recurrent_initializer='glorot_uniform',
             return_sequences=True,
             return_state=True)
+
         self.F = tf.keras.layers.Dense(vocab)
+
         self.attention = SelfAttention(units)
 
     def call(self, x, s_prev, hidden_states):
         """Summary"""
-        context_vector, _ = SelfAttention(s_prev, hidden_states)
-        x = self.embedding(x)
-        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
-        output, s = self.gru(x)
-        output = tf.reshape(output, (-1, output.shape[2]))
-        x = self.F(output)
+        context, _ = self.attention(s_prev, hidden_states)
 
-        return x, s
+        x = self.embedding(x)
+
+        context_expanded = tf.expand_dims(context, axis=1)
+        inputs = tf.concat([context_expanded, x], axis=-1)
+
+        outputs, s = self.gru(inputs=inputs)
+
+        outputs_reshaped = tf.reshape(
+            outputs, shape=(outputs.shape[0],
+                            outputs.shape[2]))
+
+        y = self.F(outputs_reshaped)
+
+        return y, s
