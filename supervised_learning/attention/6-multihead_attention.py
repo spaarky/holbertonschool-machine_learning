e801b521da7fdccecfee4d0df44233b11abe80d3
Spark4545 <lerouxpaulhenri@gmail.com>
2024-05-22 18:15:22 +0000

    [task 5] init

---
+++ supervised_learning/attention/6-multihead_attention.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Summary"""
+import tensorflow as tf
+sdp_attention = __import__('5-sdp_attention').sdp_attention
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    """Summary"""
+
+    def __init__(self, dm, h):
+        """Summary"""
+        super(MultiHeadAttention, self).__init__()
+        self.h = h
+        self.dm = dm
+        self.depth = dm // h
+        self.Wq = tf.keras.layers.Dense(dm)
+        self.Wk = tf.keras.layers.Dense(dm)
+        self.Wv = tf.keras.layers.Dense(dm)
+        self.linear = tf.keras.layers.Dense(dm)
+
+    def split_heads(self, x, batch_size):
+        """Summary"""
+        x = tf.reshape(x, (batch_size, -1, self.h, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, Q, K, V, mask):
+        """Summary"""
+        batch_size = tf.shape(Q)[0]
+
+        Q = self.Wq(Q)
+        K = self.Wk(K)
+        V = self.Wv(V)
+
+        Q = self.split_heads(Q, batch_size)
+        K = self.split_heads(K, batch_size)
+        V = self.split_heads(V, batch_size)
+
+        output, weights = sdp_attention(Q, K, V, mask)
+
+        output = tf.transpose(output, perm=[0, 2, 1, 3])
+        output = tf.reshape(output, (batch_size, -1, self.dm))
+
+        return self.linear(output)
