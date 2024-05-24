bcad5bbf8b43f9d98ce7041a8f9c2c1f5b5216b4
Spark4545 <lerouxpaulhenri@gmail.com>
2024-05-24 14:02:41 +0000

    [task 8] init

---
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Summary"""
+import tensorflow as tf
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+
+
+class Encoder(tf.keras.layers.Layer):
+    """Summary"""
+        def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
+                 drop_rate=0.1):
+        """Summary"""
+        super(Encoder, self).__init__()
+        self.N = N
+        self.dm = dm
+
+        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, self.dm)
+
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
+                       for _ in range(N)]
+
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask):
+        """Summary"""
+        seq_len = x.shape[1]
+        embedding = self.embedding(x)
+        embedding = embedding * tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        embedding = embedding + self.positional_encoding[:seq_len]
+
+        encoder_out = self.dropout(embedding, training=training)
+
+        for i in range(self.N):
+            encoder_out = self.blocks[i](encoder_out, training, mask)
+
+        return encoder_out
